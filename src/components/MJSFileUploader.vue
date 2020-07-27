ce74b1839afc90b4ab56db3bb74e4ea156ccaf9c
John Kerl <kerl.john.r@gmail.com>
2020-07-27 11:16:00 -0400

    favicon.ico

---
+++ src/components/MJSFileUploader.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="mjs-file-uploader">
+    {{ msg }}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class MJSFileUploader extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.mjs-file-uploader {
+  margin: 40px 0 0;
+  border: 1px solid #aaaaaa;
+  padding: 5px;
+}
+</style>
